e2f8725176d40bcfcc0756f5db019db4fd015066
IsacSnecker <isac.snecker@hotmail.com>
2024-04-09 19:13:16 +0200

    Added handlemethod for donatebox

--- Frontend/DAT257_Website/src/Components/DonationBox/DonationBox.tsx
+++ Frontend/DAT257_Website/src/Components/DonationBox/DonationBox.tsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
 import "./DonationBox.css";
 function DonationBox() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [donationAmount, setDonationAmount] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [cvcNumber, setCvcNumber] = useState("");
+  const [expDate, setExpDate] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const donationData = {
+      firstName,
+      lastName,
+      email,
+      donationAmount,
+      cardNumber,
+      cvcNumber,
+      expDate,
+    };
+    console.log(donationData);
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setDonationAmount("");
+    setCardNumber("");
+    setCvcNumber("");
+    setExpDate("");
+  };
+
   return (
     <>
       <div className="wrapper">
         <div className="donate rounded">
           <h2 className="mb-3"> Donate </h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group row">
               <div className="col">
                 <label htmlFor="firstName" className="form-label"></label>
@@ -13,6 +43,8 @@ function DonationBox() {
                   type="text"
                   className="form-control"
                   placeholder="First name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
                 ></input>
               </div>
 
@@ -22,6 +54,8 @@ function DonationBox() {
                   type="text"
                   className="form-control"
                   placeholder="Last name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
                 ></input>
               </div>
             </div>
@@ -33,6 +67,8 @@ function DonationBox() {
                   type="email"
                   className="form-control"
                   placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 ></input>
               </div>
 
@@ -42,6 +78,8 @@ function DonationBox() {
                   type="text"
                   className="form-control"
                   placeholder="Donation amount $"
+                  value={donationAmount}
+                  onChange={(e) => setDonationAmount(e.target.value)}
                 ></input>
               </div>
             </div>
@@ -52,6 +90,8 @@ function DonationBox() {
                 type="text"
                 className="form-control"
                 placeholder="Card Number"
+                value={cardNumber}
+                onChange={(e) => setCardNumber(e.target.value)}
               ></input>
             </div>
 
@@ -62,6 +102,8 @@ function DonationBox() {
                   type="text"
                   className="form-control"
                   placeholder="Expiration Date"
+                  value={expDate}
+                  onChange={(e) => setExpDate(e.target.value)}
                 ></input>
               </div>
 
@@ -71,6 +113,8 @@ function DonationBox() {
                   type="text"
                   className="form-control"
                   placeholder="CVC Number"
+                  value={cvcNumber}
+                  onChange={(e) => setCvcNumber(e.target.value)}
                 ></input>
               </div>
             </div>
