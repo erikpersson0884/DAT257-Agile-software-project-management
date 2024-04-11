7b28c29b489438a96a9f38e748e9b53a7f916543
Erik <erikpersson1005@gmail.com>
2024-04-11 10:05:49 +0200

    added footer component

---
+++ Frontend/DAT257_Website/src/Components/footer.tsx
@@ -0,0 +1,54 @@
+// import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function footer() {
+
+  return (
+    <footer>
+        <img src='src/assets/goal-logo.png'></img>
+        <div>
+            <h2>Sitemap</h2>
+            <ul>
+                <li>Home</li>
+                <li>News</li>
+                <li>Events</li>
+                <li>Projects</li>
+                <li>Publications</li>
+                <li>People</li>
+                <li>Links</li>
+            </ul>
+        </div>
+
+        <div>
+            <h2>Socials</h2>
+            <ul>
+                <li>
+                    <a href='instagram.com/...'>Facebook</a>
+                </li>
+                <li>
+                    <a href='instagram.com/...'>Linkedin</a>
+                </li>
+                <li>
+                    <a href='instagram.com/...'>Instgram</a>
+                </li>
+                <li>
+                    <a href='instagram.com/...'>X</a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h2>Contact</h2>
+            <ul>
+                <li>
+                    <a href="mailto:HungerZero@gmail.com">HungerZero@gmail.com</a>
+                </li>
+
+            </ul>
+        </div>
+
+    </footer>
+  )
+}
+
+export default footer
