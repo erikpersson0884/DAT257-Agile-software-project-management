07da0df1273534a6160d5dd9e046626a8e38b85a
IsacSnecker <isac.snecker@hotmail.com>
2024-04-16 00:17:42 +0200

    worked on loginform

--- Frontend/DAT257_Website/src/Components/LoginForm/LoginForm.tsx
+++ Frontend/DAT257_Website/src/Components/LoginForm/LoginForm.tsx
@@ -1,72 +1,58 @@
-import "./LoginForm.css"
+import "./LoginForm.css";
 import { useState } from "react";
 
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-
-
-const handleSubmit = (e: any) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     const donationData = {
-    email,
-    password,
+      email,
+      password,
     };
 
     console.log(donationData);
     setEmail("");
     setPassword("");
+  };
 
-};
+  const handleRegister = () => {};
 
-    return (
-        <>
+  return (
+    <>
+      <div className="loginFormContainer">
+        <div className="LoginBox">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email" className="form-label"></label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
 
-        <div className="wrapperLoginForm">
-            <div className="LoginBox">
-            <form onSubmit={handleSubmit}>
-            <div className="form-group row">
-              <div className="col">
-                <label htmlFor="email" className="form-label"></label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                ></input>
-              </div>
+            <label htmlFor="password" className="form-label"></label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
 
-
+            <div className="register-login-wrapper">
+              <p className="register"> Register </p>
+              <button type="submit" className="btn btn-success py-2 w-40 mt-2">
+                Login
+              </button>
             </div>
-
-            <div className="form-group row">
-              <div className="col">
-                <label htmlFor="password" className="form-label"></label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                ></input>
-              </div>            
-              
-              </div>
-
-              <button type="submit" className="btn btn-success py-3 w-60 mt-2">
-              Login
-            </button>
-
-              </form>
-              </div>
-              </div>
-
-        </>
-    )
-
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginForm;
