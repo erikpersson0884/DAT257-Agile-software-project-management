55b4edbb0c119fea6fd5a57ed818588c7f2e5fe0
David-Pixeln <kalmarkiwi03@gmail.com>
2024-04-19 01:30:00 +0200

    Fixed up the frontend of TopDonations, with placeholder cards

--- Frontend/DAT257_Website/src/Components/TopDonations/TopDonaitons.tsx
+++ Frontend/DAT257_Website/src/Components/TopDonations/TopDonaitons.tsx
@@ -1,16 +1,74 @@
 import { useState } from "react";
 import "./TopDonations.css";
-import image from "/src/assets/PlaceHolder.png";
+import image from "/src/assets/goal-logo.png";
 
 
 function TopDonations() {
+
+    const [isRecentSelected, setIsRecentSelected] = useState(true);
+
+    const handleRecentClick = () => {
+        setIsRecentSelected(true);
+    };
+
+    const handleTopClick = () => {
+        setIsRecentSelected(false);
+    };
+
+    const recentDonationsButton = document.getElementById('recentDonations');
+    const topDonationsButton = document.getElementById('topDonations');
+
     return(
         <>
             <div className="bodyTopDonations">
                 <div className="wrapperTopDonations">
-                    <div className="colTopDonations">
+                    <div className="titleTestTopDonations">
+                        TOP DONATIONS
+                    </div>
+                    <div className="sortingWrapperTopDonations">
+                        <div className="sortingContainerTopDonations">
+                            <div className="sortingRowTopDonations">
+                            <div className={isRecentSelected ? 'sortingSelectedTopDonations' : 'sortingDeselectedTopDonations'} onClick={handleRecentClick}>
+                                    Recent Donations
+                                </div>
+                                <div className={isRecentSelected ? 'sortingDeselectedTopDonations' : 'sortingSelectedTopDonations'} onClick={handleTopClick}>
+                                    Top Donations
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rowTopDonations">
                         <div className="cardTopDonations">
-                            Hello =P
+                            <div className="cardLogoTopDonations">
+                                <img src={image} className="cardimage" alt="PlaceHolder"></img>
+                            </div>
+                            <div className="cardTextWrapperTopDonations">
+                                <h4>
+                                    #1
+                                </h4>
+                                <p>
+                                    Anonymous
+                                </p>
+                            </div>
+                            <div className="cardAmountTopDonation">
+                                <h1>50,000kr</h1>
+                            </div>
+                        </div>
+                        <div className="cardTopDonations">
+                            <div className="cardLogoTopDonations">
+                                <img src={image} className="cardimage" alt="PlaceHolder"></img>
+                            </div>
+                            <div className="cardTextWrapperTopDonations">
+                                <h4>
+                                    #2
+                                </h4>
+                                <p>
+                                    Anonymous
+                                </p>
+                            </div>
+                            <div className="cardAmountTopDonation">
+                                <h1>20,000kr</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
