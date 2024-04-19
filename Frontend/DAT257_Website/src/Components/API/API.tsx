6d2edfa6c1921902cc53d1d75ca80a0cc18b3efe
executem <isak.o.soderlind@gmail.com>
2024-04-19 11:44:46 +0200

    Update API.tsx

--- Frontend/DAT257_Website/src/Components/API/API.tsx
+++ Frontend/DAT257_Website/src/Components/API/API.tsx
@@ -1,37 +1,35 @@
 import "./API.css";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-    const url = "https://jsonplaceholder.typicode.com/users";
-    const [data, setData] = useState([]);
+  const url = "https://api.hungermapdata.org/v1/foodsecurity/country";
+  const [data, setData] = useState([]);
 
-    const fetchInfo = () => {
-    return fetch(url)
-        .then((res) => res.json())
-        .then((d) => setData(d))
-    }
+  const fetchInfo = () => {
+    return axios.get(url).then((res) => setData(res.data.countries));
+  };
 
-
-    useEffect(() => {
+  useEffect(() => {
     fetchInfo();
-    }, []);
+  }, []);
 
   return (
     <div className="App">
-      <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
+      <h1 style={{ color: "green" }}>using Axios Library to Fetch Data</h1>
       <center>
         {data.map((dataObj, index) => {
           return (
             <div
               style={{
                 width: "15em",
-                backgroundColor: "#35D841",
+                backgroundColor: "#CD8FFD",
                 padding: 2,
                 borderRadius: 10,
                 marginBlock: 10,
               }}
             >
-              <p style={{ fontSize: 20, color: 'white' }}>{dataObj.id}</p>
+              <p style={{ fontSize: 20, color: 'white' }}>{dataObj.country.name}</p>
             </div>
           );
         })}
