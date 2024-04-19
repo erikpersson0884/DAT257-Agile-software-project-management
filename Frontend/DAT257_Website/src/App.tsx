cc6dde13c3809f03f62fb8bac09b68110dbb6fc0
IsacSnecker <isac.snecker@hotmail.com>
2024-04-19 11:20:02 +0200

    registerpage

--- Frontend/DAT257_Website/src/App.tsx
+++ Frontend/DAT257_Website/src/App.tsx
@@ -1,29 +1,25 @@
-import {
-    BrowserRouter,
-    Route,
-    Routes
-} from "react-router-dom";
-import './App.css'; 
-import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 import DonationBox from "./Components/DonationBox/DonationBox";
 import InformationPage from "./Components/InformationPage/InformationPage";
 import Footer from "./Components/Footer/Footer.tsx";
 import Header from "./Components/Header/Header.tsx";
+import RegisterPage from "./Components/RegisterPage/RegisterPage.tsx";
 
 function App() {
-    return (
-      <BrowserRouter>
-          <Header/>
-          <Routes>
-            <Route path="/information" element={<InformationPage/>}></Route>
-            <Route path="/" element={<DonationBox/>}></Route>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/information" element={<InformationPage />}></Route>
+        <Route path="/" element={<DonationBox />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+      </Routes>
 
-          </Routes>
-
-          <Footer/>
-
-      </BrowserRouter>
-    )
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
