2dd9c3974d38fec1eb5f34ae045f39bc9abab4eb
Dexetivia <79984459+Dexetivia@users.noreply.github.com>
2024-04-19 13:52:35 +0200

    Added feature title

---
+++ Frontend/DAT257_Website/src/Components/WebsiteFeature/WebsiteFeature.tsx
@@ -0,0 +1,15 @@
+import "./WebsiteFeature.css";
+
+function WebsiteFeature () {
+    return(
+            <div className = "featureImage">
+                <h1>
+                    10% of people suffering from starvation!
+                </h1>
+            </div>
+            
+           
+    )
+}
+
+export default WebsiteFeature;
