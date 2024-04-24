05f86a80f4922146912f043a428958cbd158fb54
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2024-04-24 13:25:01 +0200

    update, updated the donationbox form so that it was scalable.

--- Frontend/DAT257_Website/src/Components/DonationBox/DonationBox.tsx
+++ Frontend/DAT257_Website/src/Components/DonationBox/DonationBox.tsx
@@ -59,16 +59,6 @@ function DonationBox() {
                   value={lastName}
                   onChange={(e) => setLastName(e.target.value)}
                 ></input>
-
-                <div className="checkbox-lg">
-                  <input className="form-check-input" type="checkbox" defaultChecked={false} onChange={(e) => setIsAnonymous(!isAnonymous)}></input>
-                </div>
-
-               <div className="anonymous-label">
-               <label className="form-check-label" htmlFor="flexCheckDefault"> Make me anonymous </label>
-               </div>
-
-
             </div>
 
             <div className="form-group">
@@ -95,7 +85,7 @@ function DonationBox() {
                 <label htmlFor="cardNumber" className="form-label"></label>
                 <input
                   type="text"
-                  className="form-control"
+                  className="form-control-full"
                   placeholder="Card Number"
                   value={cardNumber}
                   onChange={(e) => setCardNumber(e.target.value)}
@@ -120,6 +110,16 @@ function DonationBox() {
                   value={cvcNumber}
                   onChange={(e) => setCvcNumber(e.target.value)}
                 ></input>
+            </div>
+
+            <div className="form-group">
+              <div className="checkbox-lg">
+                <input className="form-check-input" type="checkbox" defaultChecked={false} onChange={(e) => setIsAnonymous(!isAnonymous)}></input>
+              </div>
+
+              <div className="anonymous-label">
+              <label className="form-check-label" htmlFor="flexCheckDefault"> Make me anonymous </label>
+              </div>
             </div>
 
             <button type="submit" className="btn btn-success py-3 w-100 mt-2">
