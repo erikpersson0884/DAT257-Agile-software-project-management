705d023e582a58095c4add71124b93190813593d
Dexetivia <79984459+Dexetivia@users.noreply.github.com>
2024-04-25 13:42:14 +0200

    Styled the feature and added background

--- Frontend/DAT257_Website/src/Components/WebsiteFeature/WebsiteFeature.tsx
+++ Frontend/DAT257_Website/src/Components/WebsiteFeature/WebsiteFeature.tsx
@@ -1,11 +1,22 @@
 import "./WebsiteFeature.css";
+import {Link } from "react-router-dom";
 
 function WebsiteFeature () {
     return(
             <div className = "featureImage">
-                <h1>
-                    10% of people suffering from starvation!
-                </h1>
+                <div className="featureImageText">
+                    <h1 className="featureImageH1">
+                        10% of people are suffering <br></br> from starvation
+                    </h1>
+                    <h4>
+                        Act now!
+                    </h4>
+                    <Link to="/Donate">
+                        <button className= "btn">
+                            Donate Here!
+                        </button>
+                    </Link>
+                </div>
             </div>
             
            
