1440080125aeb67a77a675a5a2f9197124c39349
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2024-05-09 22:29:49 +0200

    feature, added switch case for switching between our different payment components

--- Frontend/DAT257_Website/src/Components/DonationBox/DonationBox.tsx
+++ Frontend/DAT257_Website/src/Components/DonationBox/DonationBox.tsx
@@ -2,21 +2,43 @@ import "./DonationBox.css";
 import KortBetal from "./KortBetal";
 import KlarnaBetal from "./KlarnaBetal";
 import SwishBetal from "./SwishBetal";
+import { useState } from 'react';
+
+
 
 function DonationBox() {
+  const [showComponent, setShowComponent] = useState('A');
+
+  const handleClick = (showComponent) => {
+    setShowComponent(showComponent);
+  }
+
+  let componentToDisplay;
+
+  switch (showComponent) {
+    case 'A':
+      componentToDisplay = <KortBetal />;
+      break;
+    case 'B':
+      componentToDisplay = <KlarnaBetal />;
+      break;
+    case 'C':
+      componentToDisplay = <SwishBetal />;
+      break;
+    default:
+      componentToDisplay = <KortBetal />;
+  }
   return (
     <>
       <div className="wrapperDonationBox">
         <div className="donate rounded">
           <h2 className="mb-3"> Donate </h2>
-            {/* <KortBetal>
-            </KortBetal> */}
-            {/* <KlarnaBetal>
-              
-            </KlarnaBetal> */}
-            <SwishBetal>
-
-            </SwishBetal>
+          <div className = "paymentButtons">
+            <button className="btn btn-primary btn-lg" id="kort" onClick={() => handleClick('A')}> Kort</button>
+            <button className="btn btn-primary btn-lg" id="klarna" onClick={() => handleClick('B')}> Klarna </button>
+            <button className="btn btn-primary btn-lg" id="swish" onClick={() => handleClick('C')}> Swish </button>
+          </div>
+          {componentToDisplay}
         </div>
       </div>
     </>
