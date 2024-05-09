a0fe964d5abbcdd7436b3d60588c6dc28a9f5e51
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2024-05-09 18:12:14 +0200

    feature, added anonymous button

--- Frontend/DAT257_Website/src/Components/DonationBox/KlarnaBetal.tsx
+++ Frontend/DAT257_Website/src/Components/DonationBox/KlarnaBetal.tsx
@@ -4,12 +4,14 @@ function KlarnaBetal() {
     const [email, setEmail] = useState("");
     const [postNumber, setPostNumber] = useState("");
     const [donationAmount, setDonationAmount] = useState("");
+    const [isAnonymous, setIsAnonymous] = useState(Boolean);
     const handleSubmit = (e: any) => {
     e.preventDefault();
     const donationData = {
         email,
         postNumber,
-        donationAmount
+        donationAmount,
+        isAnonymous
     };
     console.log(donationData);
     setEmail("");
@@ -40,15 +42,29 @@ function KlarnaBetal() {
             </div>
 
             <div className="form-group">
-                    <label htmlFor="cardNumber" className="form-label"></label>
-                    <input
-                    type="text"
-                    className="form-control-full"
-                    placeholder="Card Number"
-                    value={donationAmount}
-                    onChange={(e) => setDonationAmount(e.target.value)}
-                    ></input>
+                <label htmlFor="cardNumber" className="form-label"></label>
+                <input
+                type="text"
+                className="form-control-full"
+                placeholder="Card Number"
+                value={donationAmount}
+                onChange={(e) => setDonationAmount(e.target.value)}
+                ></input>
+            </div>
+
+            <div className="form-group">
+                <div className="checkbox-lg">
+                    <input className="form-check-input" type="checkbox" defaultChecked={false} onChange={(e) => setIsAnonymous(!isAnonymous)}></input>
                 </div>
+
+                <div className="anonymous-label">
+                    <label className="form-check-label" htmlFor="flexCheckDefault"> Make me anonymous </label>
+                </div>
+            </div>
+
+            <button type="submit" className="btn btn-success py-3 w-100 mt-2">
+                Donate
+            </button>
         </form>
         </>
     )
